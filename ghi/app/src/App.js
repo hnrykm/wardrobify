83ba7a4dc0092c6003d7ab3aa7b8dde8a6ab1fda
Henry Kim <hnrykm@gmail.com>
2023-06-02 22:49:02 -0400

    App.js merge conflict fixed

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+
 import HatForm from './HatForm';
 import HatsList from './HatsList';
+import HatFormEdit from './HatFormEdit';
+
 import ShoeList from './ShoeList';
+import NewShoeForm from './ShoeForm';
 
 function App() {
 	return (
@@ -16,9 +20,11 @@ function App() {
           <Route path="hats">
             <Route index element={<HatsList />} />
             <Route path="new" element={<HatForm />} />
+			<Route path="edit/:id" element={<HatFormEdit />} />
           </Route>
           <Route path="shoes">
             <Route index element={<ShoeList/>}/>
+			<Route path="new" element={<NewShoeForm />} />
           </Route>
         </Routes>
 
